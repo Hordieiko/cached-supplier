6fd69e6a41cc6d41c40567928f683528f4a53163
Vasyl Hordieiko <pemiker@gmail.com>
2024-01-24 17:25:02 +0200

    updated tests

--- src/test/java/io/github/hordieiko/CachedSupplierTest.java
+++ src/test/java/io/github/hordieiko/CachedSupplierTest.java
@@ -1,51 +1,44 @@
 package io.github.hordieiko;
 
+import lombok.Getter;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import java.lang.ref.Reference;
 import java.time.Duration;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class CachedSupplierTest {
 
-    private static final Duration TIMEOUT_ONE_SEC = Duration.ofSeconds(1L);
-    private static final Duration TECHNICAL_TIMEOUT = Duration.ofMillis(200L);
+    private static final Duration TIMEOUT = Duration.ofMillis(100L);
 
     /**
      * An expired value must be closed even if its wrapper has not been closed properly
      */
     @Test
     void testUnreferencedWrapperCleaned() {
-        final var timeout = TIMEOUT_ONE_SEC;
-        final var executorSupplier = CachedSupplier.of(Executors::newSingleThreadExecutor, timeout, ExecutorService::shutdown);
+        final var cachedSupplier = CachedSupplier.of(CloseableService::new, TIMEOUT, CloseableService::close);
 
-        var executorWrapper = executorSupplier.get();
-        final var executor = executorWrapper.get();
+        var wrapper = cachedSupplier.get();
+        final var value = wrapper.get();
 
-        Awaitility.await()
-                .timeout(timeout)
-                .pollDelay(timeout.minusMillis(1))
-                .until(() -> true);
+        waitForAWhile();
 
-        executorWrapper = null;
-        Reference.reachabilityFence(executorWrapper);
+        wrapper = null;
+        Reference.reachabilityFence(wrapper);
 
         Awaitility.await()
                 .pollInterval(Duration.ofMillis(10))
-                .untilAsserted(() -> {System.gc(); assertTrue(executor::isShutdown);});
+                .untilAsserted(() -> {System.gc(); assertTrue(value.isClosed());});
     }
 
     /**
@@ -53,18 +46,17 @@ class CachedSupplierTest {
      */
     @Test
     void testCachedValueClosed() {
-        final var timeout = TIMEOUT_ONE_SEC;
-        final var executorSupplier = CachedSupplier.of(Executors::newSingleThreadExecutor, timeout, ExecutorService::shutdown);
+        final var cachedSupplier = CachedSupplier.of(CloseableService::new, TIMEOUT, CloseableService::close);
 
-        final ExecutorService executor;
-        try (var executorWrapper = executorSupplier.get()) {
-            executor = executorWrapper.get();
-            waitForAWhile(timeout);
+        final CloseableService value;
+        try (var wrapper = cachedSupplier.get()) {
+            value = wrapper.get();
+            waitForAWhile();
         }
 
-        Awaitility.waitAtMost(TECHNICAL_TIMEOUT)
+        Awaitility.waitAtMost(TIMEOUT)
                 .pollInterval(Duration.ofMillis(10))
-                .until(executor::isShutdown);
+                .until(value::isClosed);
     }
 
     /**
@@ -73,45 +65,44 @@ class CachedSupplierTest {
      */
     @Test
     void testSupplierProducesTemporaryCachedValue() {
-        final var timeout = TIMEOUT_ONE_SEC;
-        final var executorSupplier = CachedSupplier.of(Executors::newSingleThreadExecutor, timeout, ExecutorService::shutdown);
+        final var cachedSupplier = CachedSupplier.of(CloseableService::new, TIMEOUT, CloseableService::close);
 
-        final var wrapper1 = executorSupplier.get();
-        final var wrapper2 = executorSupplier.get();
-        final ExecutorService executor1;
+        final var wrapper1 = cachedSupplier.get();
+        final var wrapper2 = cachedSupplier.get();
+        final CloseableService value1;
         try (wrapper1) {
-            executor1 = wrapper1.get();
-            final var executor2 = wrapper1.get();
-            final var executor3 = wrapper1.get();
+            value1 = wrapper1.get();
+            final var value2 = wrapper1.get();
+            final var value3 = wrapper1.get();
             // the value is cached inside the wrapper
-            assertEquals(executor2, executor3);
+            assertSame(value2, value3);
         }
 
         // different wrappers have one cached value during the cached time
-        assertNotEquals(wrapper1, wrapper2);
-        final var executor4 = wrapper2.get();
-        assertEquals(executor1, executor4);
+        assertNotSame(wrapper1, wrapper2);
+        final var value4 = wrapper2.get();
+        assertSame(value1, value4);
 
-        waitForAWhile(timeout);
+        waitForAWhile();
 
         // until all wrappers with expired value are not closed/cleaned by the GS the value is safe to use
-        final Executable executable = () -> executor1.submit(() -> {});
+        final Executable executable = value1::execute;
         assertDoesNotThrow(executable);
 
         wrapper2.close(); // after a timeout to become expired at the time of the close
 
         // if a value has expired and all its wrappers have been closed, it is impossible to use it anymore
-        assertThrows(RejectedExecutionException.class, executable);
-        assertTrue(executor1.isShutdown());
+        assertThrows(IllegalStateException.class, executable);
+        assertTrue(value1.isClosed());
 
         // expired wrappers still contains their cached values even after they have been closed
-        final var executor5 = wrapper2.get();
-        assertEquals(executor1, executor5);
+        final var value5 = wrapper2.get();
+        assertSame(value1, value5);
 
-        try (final var closeableValue = executorSupplier.get()) {
-            final var executor6 = closeableValue.get();
+        try (final var wrapper = cachedSupplier.get()) {
+            final var value6 = wrapper.get();
             // once a cached value has expired the supplier renews it with a new one
-            assertNotEquals(executor1, executor6);
+            assertNotSame(value1, value6);
         }
     }
 
@@ -121,47 +112,58 @@ class CachedSupplierTest {
      */
     @Test
     void testBuilder() {
-        final var timeout = TIMEOUT_ONE_SEC;
-        final var executorSupplierWithFinisher = CachedSupplier.builder(Executors::newSingleThreadExecutor)
-                .duration(timeout)
-                .finisher(ExecutorService::shutdown)
-                .build();
+        final var supplierWithFinisher =
+                CachedSupplier.builder(CloseableService::new)
+                        .duration(TIMEOUT)
+                        .finisher(CloseableService::close)
+                        .build();
 
-        final ExecutorService executor1;
-        try (final var executorWrapper = executorSupplierWithFinisher.get()) {
-            executor1 = executorWrapper.get();
-            waitForAWhile(timeout);
-            assertFalse(executor1.isShutdown());
+        final CloseableService value1;
+        try (final var wrapper = supplierWithFinisher.get()) {
+            value1 = wrapper.get();
+            waitForAWhile();
+            assertFalse(value1.isClosed());
         }
 
-        Awaitility.waitAtMost(TECHNICAL_TIMEOUT)
+        Awaitility.waitAtMost(TIMEOUT)
                 .pollInterval(Duration.ofMillis(10))
-                .until(executor1::isShutdown); // the finisher function has been applied so the executor is shut down
+                .until(value1::isClosed); // the finisher function has been applied so the service is closed
 
-        final var executorSupplierWithoutFinisher = CachedSupplier.builder(Executors::newSingleThreadExecutor)
-                .duration(timeout)
-                .build();
+        final var supplierWithoutFinisher =
+                CachedSupplier.builder(CloseableService::new)
+                        .duration(TIMEOUT)
+                        .build();
 
-        final ExecutorService executor2;
-        try (final var executorWrapper = executorSupplierWithoutFinisher.get()) {
-            executor2 = executorWrapper.get();
-            waitForAWhile(timeout);
-            assertFalse(executor2.isShutdown());
+        final CloseableService value2;
+        try (final var wrapper = supplierWithoutFinisher.get()) {
+            value2 = wrapper.get();
+            waitForAWhile();
+            assertFalse(value2.isClosed());
         }
 
-        Awaitility.waitAtMost(TECHNICAL_TIMEOUT)
+        Awaitility.waitAtMost(TIMEOUT)
                 .pollInterval(Duration.ofMillis(10))
-                .untilAsserted(() -> assertFalse(executor2.isShutdown())); // there is no finisher function, so the executor is still active
+                .untilAsserted(() -> assertFalse(value2.isClosed())); // there is no finisher function, so the service is still active
 
         // an instance of CachedSupplier with predefined duration and finisher
-        final var executorSupplier = CachedSupplier.builder(Executors::newSingleThreadExecutor).build();
-        assertNotNull(executorSupplier);
+        final var supplier = CachedSupplier.builder(CloseableService::new).build();
+        assertNotNull(supplier);
     }
 
-    private static void waitForAWhile(final Duration timeout) {
+    private static void waitForAWhile() {
         Awaitility.await()
-                .timeout(timeout)
-                .pollDelay(timeout.minusMillis(1))
+                .timeout(TIMEOUT)
+                .pollDelay(TIMEOUT.minusMillis(1))
                 .until(() -> true);
+    }
+
+    @Getter
+    private static final class CloseableService implements AutoCloseable {
+        volatile boolean closed;
+
+        void execute() throws IllegalStateException {if (closed) throw new IllegalStateException();}
+
+        @Override
+        public void close() {closed = true;}
     }
 }
